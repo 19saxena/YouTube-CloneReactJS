c537079d56d0b69d7bf618ee7857d2d1537133ef
Sahaj Saxena <sahajsaxena1906@gmail.com>
2024-03-24 23:02:37 +0530

    src

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 import Home from './Home';
 import About from './About';
 import Header from './Header';
+import Explore from './Explore'
 import Sidebar from './components/sidebar/Sidebar';
+import VideoPlayer from './VideoPlayer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
@@ -12,8 +15,18 @@ function App() {
       <div className='app_mainpage'>
         <BrowserRouter>
         <Routes>
-          <Route path='/' element={<><Header/><div><Sidebar></Sidebar></div></>} />
-          <Recom
+        <Route path='/' element={<><Header/><div><Sidebar></Sidebar><Explore/></div></>} />
+
+          
+          <Route path='/video/:videoId' element={
+            <>
+              <Header />
+              <div className="app__mainpage">
+                <Sidebar />
+                <VideoPlayer />
+              </div>
+            </>
+          } />
         </Routes>
         </BrowserRouter>
       </div>
